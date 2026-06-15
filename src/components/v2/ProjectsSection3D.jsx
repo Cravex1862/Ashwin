6677a7b6111b5f98a6f8cc3bed454e975c5f4c96
Cravex1862 <ashwinchoudhury1310@gmail.com>
2026-06-15 21:30:11 +0530

    fix: remove unused import to fix vercel build

--- src/components/v2/ProjectsSection3D.jsx
+++ src/components/v2/ProjectsSection3D.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { playClickSound } from '../../utils/audio';
 
 export default function ProjectsSection3D({ phase, projects, onProjectClick }) {
